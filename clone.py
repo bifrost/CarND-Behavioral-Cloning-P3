38af23ad9edc0b83e04ecfbd0ca0dc78b6a50c4f
Dan Rasmussen <dan@steenbjerg.com>
2018-08-29 21:25:47 +0200

    first model

---
+++ clone.py
@@ -0,0 +1,24 @@
+import csv
+import cv2
+import numpy as np
+
+lines = []
+with open('./data/driving_log.csv') as csvFile:
+    raeder = csv.reader(csvFile)
+    for line in reader:
+        lines.append(line)
+        
+        
+images = []
+measurements = []
+for line in lines:
+    source_path = line[0]
+    filename = source_path.split('/')[-1]
+    current_path = './data/IMG/' + filename
+    image = cv2.imread(current_path)
+    images.append(image)
+    measurement = float(line[3])
+    measurements.append(measurement)
+    
+X_train = np.array(images)
+Y_train = np.array(measurements)
